43b7cfac4ff19acb26435548ac489f56d37eda93
SoloDuck <colesjulius2@gmail.com>
2024-11-26 22:23:20 +0800

    pre-dep V: another try

--- app.py
+++ app.py
@@ -934,6 +934,8 @@ def init_app(app):
 
 if __name__ == '__main__':
     init_app(app)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
     app.run(debug=True)
 else:
     init_app(app)
